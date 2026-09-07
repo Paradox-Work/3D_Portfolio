094b83fe2385173c95103b7ff5cd1f453581d7b0
Paradox-Work <ipa23.a.vasiljevs@vtdt.edu.lv>
2026-09-08 01:26:45 +0300

    Added Feature Cards sections and created Experience file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import LogoSection from './components/LogoSection.jsx';
 import NavBar from './components/NavBar.jsx'
+import ExperienceSection from './sections/ExperienceSection.jsx';
+import FeatureCards from './sections/FeatureCards.jsx';
 import Hero from './sections/Hero'
 import ShowcaseSection from './sections/ShowcaseSection';
 
@@ -11,6 +13,8 @@ const App = () => {
       <Hero />
       <ShowcaseSection />
       <LogoSection />
+      <FeatureCards />
+      <ExperienceSection />
     </>
   )
 }
